3ad0f24b17f2798521f46186ea8ff5fcdba9af88
Aravena <134702678+smaravena@users.noreply.github.com>
2024-12-16 08:13:28 -0300

    Revert "test new url"
    This reverts commit 460b77da436f29003f5b23baa84fa3abf2dc1830.

--- js/cta-detect.js
+++ js/cta-detect.js
@@ -11,7 +11,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   // Replace div depending of userAgent
   if (isMobile) {
-    ctaContainer.innerHTML = `<a href="https://github.com/MatiasGonzalezPizarro/Upliftly/releases/latest/download/app-release.apk" class="bg-tertiary text-white py-3 px-6 rounded-md text-lg hover:bg-primary transition flex items-center justify-center">
+    ctaContainer.innerHTML = `<a href="https://github.com/MatiasGonzalezPizarro/Upliftly/releases" class="bg-tertiary text-white py-3 px-6 rounded-md text-lg hover:bg-primary transition flex items-center justify-center">
          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
          </svg>
